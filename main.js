7f050440ba3824dd9f50d77dc9534e3994385b66
janath <janathhma@gmail.com>
2024-03-28 06:09:13 +0530

    Add css flex boxes

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const [app,BrowserWindow] = require('electrone');
-
+const { app, BrowserWindow } = require('electron');
 function createWindow(){
     const win = new BrowserWindow({
         width: 600,
